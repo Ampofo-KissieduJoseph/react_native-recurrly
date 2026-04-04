d3422c5b95265de8f9ee74b80534b26b41bedecd
Ampofo-KissieduJoseph <josephampofo509@gmail.com>
2026-04-04 02:19:53 +0100

    fix bugs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import "@/global.css";
 import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 import {styled} from "nativewind";
